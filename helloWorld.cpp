385a305f0a0fac5a755e833c84889ecc5196527f
Bishwo <_bish@arjuns-MacBook-Air.local>
2022-04-17 22:33:26 +0545

    Just checking
    noting

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,1 +1,24 @@
 //this is random file
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Name{
+    string realName;
+    public:
+        void getData(){
+            cout<<"Enter the real name: ";
+            cin>>realName;
+        }
+        void displayName(){
+            cout<<"The real entered name is : "<<realName<<endl;
+        }
+
+};
+int main(){
+    Name n1;
+    n1.getData();
+    n1.displayName();
+    return 0;
+
+}
